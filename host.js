dc01e80756481f669568a7b21f6e73728baecf32
Tejas Andhare <138149131+tejasandhare@users.noreply.github.com>
2023-08-06 15:07:23 +0530

    Create host.js

---
+++ host.js
@@ -0,0 +1,28 @@
+let movies = [
+    {
+        name: "Falcon and Winter Soldier",
+        des: "iasdiof aouiweior ddjkjal djjo woodk uwoioe jddw duuwojja dljkeiuue fjjdoeu kjkaue iuuiwkjjkd iwie",
+        image: "F:/VSProgram/HOTSTAR/HOTSTAR CLONE/Images/slider 2.PNG"
+    },
+    {
+        name: "Loki",
+        des: "iasdiof aouiweior ddjkjal djjo woodk uwoioe jddw duuwojja dljkeiuue fjjdoeu kjkaue iuuiwkjjkd iwie",
+        image: "F:/VSProgram/HOTSTAR/HOTSTAR CLONE/Images/slider 1.PNG"
+    },
+    {
+        name: "Wanda Vision",
+        des: "iasdiof aouiweior ddjkjal djjo woodk uwoioe jddw duuwojja dljkeiuue fjjdoeu kjkaue iuuiwkjjkd iwie",
+        image: "F:/VSProgram/HOTSTAR/HOTSTAR CLONE/Images/slider 3.PNG"
+    },
+    {
+        name: "Raya and the Last Dragon",
+        des: "iasdiof aouiweior ddjkjal djjo woodk uwoioe jddw duuwojja dljkeiuue fjjdoeu kjkaue iuuiwkjjkd iwie",
+        image: "F:/VSProgram/HOTSTAR/HOTSTAR CLONE/Images/slider 4.PNG"
+    },
+    {
+        name: "Luca",
+        des: "iasdiof aouiweior ddjkjal djjo woodk uwoioe jddw duuwojja dljkeiuue fjjdoeu kjkaue iuuiwkjjkd iwie",
+        image: "F:/VSProgram/HOTSTAR/HOTSTAR CLONE/Images/slider 5.PNG"
+    }
+    ];
+    
